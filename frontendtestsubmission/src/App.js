d6223f865949d834426fb7e98f3d48b3411b33f9
AN-06 <aleena61203@gmail.com>
2025-09-08 14:28:19 +0530

    add router and wire pages in App

--- frontendtestsubmission/src/App.js
+++ frontendtestsubmission/src/App.js
@@ -1,40 +1,23 @@
 // FrontendTestSubmission/src/App.js
-import React, { useState } from "react";
-import { Log } from "./utils/loggingClient";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ShortenerPage from "./pages/ShortenerPage";
+import StatsPage from "./pages/StatsPage";
+import RedirectHandler from "./pages/RedirectHandler";
 
-function App() {
-  const [status, setStatus] = useState("");
-
-  async function sendFrontendLog() {
-    setStatus("sending...");
-    try {
-      const res = await Log("frontend","info","api","Test frontend log from React");
-      setStatus("ok: " + JSON.stringify(res));
-    } catch (e) {
-      setStatus("error: " + e.message);
-    }
-  }
-
-  async function sendInvalidLog() {
-    setStatus("sending invalid...");
-    try {
-      // deliberately invalid package to test error/logging
-      await Log("frontend","info","handler","this should fail package validation on server maybe");
-      setStatus("ok");
-    } catch (e) {
-      setStatus("error: " + e.message);
-    }
-  }
-
+function App(){
   return (
-    <div style={{ padding: 24 }}>
-      <h1>Logging Test</h1>
-      <button onClick={sendFrontendLog} style={{ marginRight: 8 }}>Send frontend log</button>
-      <button onClick={sendInvalidLog}>Send invalid log</button>
-      <pre style={{ marginTop: 12 }}>{status}</pre>
-      <p>Make sure you created FrontendTestSubmission/.env locally with REACT_APP_LOG_API_TOKEN</p>
-    </div>
+    <Router>
+      <nav style={{padding:12}}>
+        <Link to="/">Shorten</Link>{" | "}
+        <Link to="/stats">Statistics</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ShortenerPage/>} />
+        <Route path="/stats" element={<StatsPage/>} />
+        <Route path="/r/:shortcode" element={<RedirectHandler/>} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
